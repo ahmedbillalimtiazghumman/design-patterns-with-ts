d2910245080383d0dc69ce9f6d5a9e91ef1faa89
ahmedbilal1994 <ahmedbillalimtiazghumman@gmail.com>
2022-06-24 01:43:52 +0500

    SOILD principle

---
+++ solid.js
@@ -0,0 +1,29 @@
+/*
+*                                    ****SOLID PRINCIPLE****
+* A class must have a single primary responsibility. It should only have one reason to change that
+* reason being somehow related to change its responsibility. In other words it is a bad idea to add
+* more than one responsibility to a class.
+*
+* */
+var Journal = /** @class */ (function () {
+    function Journal() {
+        this.entries = {};
+        this.entries = {};
+        this.count = 0;
+    }
+    Journal.prototype.addEntries = function (text) {
+        ++this.count;
+        console.log(this.count);
+        this.entries[this.count] = "".concat(this.count, ": ").concat(text);
+    };
+    Journal.prototype.removeEntry = function (index) {
+        delete this.entries[index];
+    };
+    return Journal;
+}());
+var journal = new Journal();
+journal.addEntries('this is shit');
+journal.addEntries('this is shit1');
+journal.addEntries('this is shit2');
+journal.addEntries('this is shit3');
+console.log(journal);
---
+++ solid.ts
@@ -0,0 +1,36 @@
+/*
+*                                    ****SOLID PRINCIPLE****
+* A class must have a single primary responsibility. It should only have one reason to change that
+* reason being somehow related to change its responsibility. In other words it is a bad idea to add
+* more than one responsibility to a class.
+*
+* */
+
+
+class Journal {
+    private readonly entries = <any>{};
+    private count: number = 0;
+
+    constructor() {
+    }
+
+    addEntries(text: string) {
+        ++this.count;
+        console.log(this.count);
+        this.entries[this.count] = `${this.count}: ${text}`;
+    }
+
+    removeEntry(index: number) {
+        delete this.entries[index];
+    }
+}
+
+
+const journal = new Journal();
+
+journal.addEntries('this is shit');
+journal.addEntries('this is shit1');
+journal.addEntries('this is shit2');
+journal.addEntries('this is shit3');
+
+console.log(journal);
